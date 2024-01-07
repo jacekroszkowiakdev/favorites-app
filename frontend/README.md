e7d36196b0f7de7763590e3aaf054c25fc380614
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-01-08 00:34:01 +0100

    feat: initial commit

---
+++ frontend/README.md
@@ -0,0 +1,13 @@
+## Project created with React + TypeScript + Vite.
+
+### Configuration
+
+1.  Create a .env file in the frontend folder and define the following variable:
+
+    ```env
+    VITE_PRODUCTS_DB_PORT=your_desired_port_number
+    ```
+
+2.  In project root directory run `npm i` to install all the dependencies.
+
+3.  To start the project run `npm run dev` and **ctrl+click** to on the link to open in the browser.
---
+++ frontend/src/App.tsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ProductList } from "./components/ProductsList.component";
+import { Product } from "./model/model";
+
+const App: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+    const productsDBPort = import.meta.env.VITE_PRODUCTS_BACKEND_SERVER_PORT;
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(
+                `http://localhost:${productsDBPort}/api/products`
+            );
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json();
+            setProducts(data.products);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+            <h2>Click the button to see the products</h2>
+            <ProductList products={products} />
+        </>
+    );
+};
+
+export default App;
